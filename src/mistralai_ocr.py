997585c46e75f7b264aa6d5949357bdb401308b1
kento2247 <tkento1985@icloud.com>
2025-03-19 23:26:15 +0900

    add images

--- src/mistralai_ocr.py
+++ src/mistralai_ocr.py
@@ -1,6 +1,9 @@
+import base64
 import os
+import re
 
 from mistralai import Mistral
+from mistralai.models import OCRPageObject, OCRResponse
 
 
 class MistralOCR:
@@ -8,19 +11,32 @@ class MistralOCR:
         self.client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
 
     def ocr(self, image_url: str):
-        ocr_response = self.client.ocr.process(
+        pages: OCRResponse = self.client.ocr.process(
             model="mistral-ocr-latest",
             document={
                 "type": "image_url",
                 "image_url": image_url,
             },
         )
-        return ocr_response
+        page: OCRPageObject = pages.pages[0]
+        return page.markdown
+
+    def encode_image(self, image_path):
+        """Encode the image to base64."""
+        try:
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode("utf-8")
+        except FileNotFoundError:
+            print(f"Error: The file {image_path} was not found.")
+            return None
+        except Exception as e:  # Added general exception handling
+            print(f"Error: {e}")
+            return None
 
 
 if __name__ == "__main__":
     mistral_ocr = MistralOCR()
-    ocr_response = mistral_ocr.ocr(
-        "https://raw.githubusercontent.com/mistralai/cookbook/refs/heads/main/mistral/ocr/receipt.png"
-    )
-    print(ocr_response)
+    base64_image = mistral_ocr.encode_image("images/0.jpg")
+    image_url = f"data:image/jpeg;base64,{base64_image}"
+    markdown = mistral_ocr.ocr(image_url)
+    print(markdown)
