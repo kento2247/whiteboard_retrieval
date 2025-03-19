631627fb7afe9f750d31614674e71825c94191fb
kento2247 <tkento1985@icloud.com>
2025-03-19 22:59:40 +0900

    up

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
 from pydantic import BaseModel
-
 
 app = FastAPI()
 
 # "static" フォルダを静的ファイル用にマウント
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 # データモデル（例：画像とテキストペア）
 class ImageTextPair(BaseModel):
@@ -21,13 +22,16 @@ class ImageTextPair(BaseModel):
 async def read_root():
     return FileResponse("static/index.html")
 
+
 @app.get("/search", response_class=FileResponse)
 async def read_search():
     return FileResponse("static/search.html")
 
+
 @app.get("/record", response_class=FileResponse)
 async def read_record():
     return FileResponse("static/record.html")
+
 
 @app.get("/list", response_class=FileResponse)
 async def read_list():
