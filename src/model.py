e413ff876e72906fcc58b39525777b79b0838d71
kento2247 <tkento1985@icloud.com>
2025-03-19 23:54:09 +0900

    up

---
+++ src/model.py
@@ -0,0 +1,2 @@
+from src.mistralai_api import DescribedImage, MistralModel
+from src.openai_api import NamedEntities, OpenaiApiClient
--- src/stella.py
+++ src/stella.py
@@ -1,17 +1,21 @@
-from sentence_transformers import SentenceTransformer
 import numpy as np
 import torch
+from sentence_transformers import SentenceTransformer
 
-class stella_embedder:
+
+class StellaEmbedder:
     def __init__(self):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.model = SentenceTransformer("dunzhang/stella_en_400M_v5", trust_remote_code=True).to(self.device)
+        self.model = SentenceTransformer(
+            "dunzhang/stella_en_400M_v5", trust_remote_code=True
+        ).to(self.device)
 
     def embed_text(self, text):
         result = self.model.encode(text)  ## stellaの出力は1024次元
         return np.array(result)
-    
+
+
 if __name__ == "__main__":
-    embedder = stella_embedder()
+    embedder = StellaEmbedder()
     text = "I am a student."
     print(embedder.embed_text(text).shape)  ## (1024,)
