35475f7d226899b8d65b53f2f5abb0d29b771bac
kento2247 <tkento1985@icloud.com>
2025-03-20 20:37:28 +0900

    fix path

--- src/static/js/list.js
+++ src/static/js/list.js
@@ -1,32 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const imagesContainer = document.querySelector(".images");
+  const imagesContainer = document.querySelector(".images");
 
-    function fetchImageData() {
-        return [
-            { text: "Image 1", src: "/static/images/1.jpg" },
-            { text: "Image 2", src: "/static/images/2.jpg" },
-            { text: "Image 3", src: "/static/images/3.jpg" },
-            { text: "Image 4", src: "/static/images/4.jpg" },
-        ];
-    }
+  function fetchImageData() {
+    return [
+      { text: "Image 1", src: "/static/uploads/1.jpg" },
+      { text: "Image 2", src: "/static/uploads/2.jpg" },
+      { text: "Image 3", src: "/static/uploads/3.jpg" },
+      { text: "Image 4", src: "/static/uploads/4.jpg" },
+      { text: "Image 5", src: "/static/uploads/5.jpg" },
+    ];
+  }
 
-    function addImageBlock(data) {
-        const blockDiv = document.createElement("div");
-        blockDiv.classList.add("image-block");
+  function addImageBlock(data) {
+    const blockDiv = document.createElement("div");
+    blockDiv.classList.add("image-block");
 
-        const img = document.createElement("img");
-        img.src = data.src;
-        img.alt = data.text;
-        blockDiv.appendChild(img);
+    const img = document.createElement("img");
+    img.src = data.src;
+    img.alt = data.text;
+    blockDiv.appendChild(img);
 
-        const description = document.createElement("p");
-        description.classList.add("description");
-        description.textContent = data.text;
-        blockDiv.appendChild(description);
+    const description = document.createElement("p");
+    description.classList.add("description");
+    description.textContent = data.text;
+    blockDiv.appendChild(description);
 
-        imagesContainer.appendChild(blockDiv);
-    }
+    imagesContainer.appendChild(blockDiv);
+  }
 
-    const imageData = fetchImageData();
-    imageData.forEach(addImageBlock);
+  const imageData = fetchImageData();
+  imageData.forEach(addImageBlock);
 });
